0bf02b0f3643489f539e3cd4d28dd1f7b9aa1adf
Alexander Zaretskii <al.zaretskii@gmail.com>
2016-01-22 18:32:31 +0200

    fix Oauth

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 var Linkedin = require('node-linkedin')(),
-    _ = require('lodash'),
     util = require('./util.js');
 
 var pickInputs = {
-        'id': 'id',
-        'updateKey': 'updateKey',
-        'comment': 'comment'
+        'id': { key: 'id', validate: { req: true } },
+        'updateKey': { key: 'updateKey', validate: { req: true } },
+        'comment': { key: 'comment', validate: { req: true } }
     },
     pickOutputs = {
         'id': 'id',
@@ -15,24 +14,6 @@ var pickInputs = {
     };
 
 module.exports = {
-
-    /**
-     * Authorize module.
-     *
-     * @param dexter
-     * @returns {*}
-     */
-    authModule: function (dexter) {
-        var accessToken = dexter.environment('linkedin_access_token');
-
-        if (accessToken)
-            return Linkedin.init(accessToken);
-
-        else
-            return false;
-    },
-
-
     /**
      * The main entry point for the Dexter module
      *
@@ -40,32 +21,16 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var linkedIn = this.authModule(dexter),
-            inputs = util.pickStringInputs(step, pickInputs);
+        var inputs = util.pickInputs(step, pickInputs),
+            linkedIn = Linkedin.init(dexter.provider('linkedin').credentials('access_token')),
+            apiURL = 'https://api.linkedin.com/v1/companies/' + inputs.id + '/updates/key=' + inputs.updateKey + '/update-comments-as-company/';
 
-        if (!linkedIn)
-            return this.fail('A [linkedin_access_token] environment need for this module.');
+        linkedIn.companies.createCall('POST', apiURL, {comment: inputs.comment}, function(err, data) {
+            if (err || (data && data.errorCode !== undefined))
+                this.fail(err || (data.message || 'Error Code: '.concat(data.errorCode)));
+            else
+                this.complete(util.pickOutputs(data, pickOutputs));
 
-        if (!inputs.id)
-            this.fail('A [id] need for this module');
-
-        linkedIn.companies.createCall(
-            'POST',
-            'https://api.linkedin.com/v1/companies/' + inputs.id + '/updates/key=' + inputs.updateKey + '/update-comments-as-company/',
-            {
-                comment: inputs.comment
-            },
-            function(err, data) {
-                if (err)
-                    this.fail(err);
-
-                else if (data.errorCode !== undefined)
-                    this.fail(data.message || 'Error Code'.concat(data.errorCode));
-
-                else
-                    this.complete(util.pickResult(data, pickOutputs));
-
-            }.bind(this)
-        )(linkedIn.companies.config);
+        }.bind(this))(linkedIn.companies.config);
     }
 };
